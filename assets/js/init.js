fb9c4b82028683d263f858360d01b0ce97da0208
Eddy Simancas <edylber.simancas@outlook.com>
2025-05-03 20:44:58 -0500

    Maquetando el hero de index.html

--- assets/js/init.js
+++ assets/js/init.js
@@ -3,6 +3,17 @@
 
     $('.sidenav').sidenav();
     $('.tabs').tabs();
+    $('.materialboxed').materialbox();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+
+    // Function to move to the next slide every 3 seconds
+    setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 3000); // Adjust the interval as needed
+  
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
