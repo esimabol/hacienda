1d55a42007ec06f05b48df624a7cb228aed95f81
Eddy Simancas <edylber.simancas@outlook.com>
2025-05-28 21:57:38 -0500

    Iniciando el Directorio

--- assets/js/init.js
+++ assets/js/init.js
@@ -10,10 +10,26 @@
     });    
      $('.tooltipped').tooltip();
      $('.modal').modal();
-    // Function to move to the next slide every 3 seconds
-    // setInterval(function() {
-    //   $('.carousel').carousel('next');
-    // }, 3000);
+    
+     function ingreso() {
+        let fecha = new Date();
+        let mes = fecha.getMonth() + 1;
+        let hora = fecha.getHours();
+
+        let porteria1Abierta = mes % 2 === 0 || (hora >= 6 && hora < 22);
+        let porteria2Abierta = mes % 2 !== 0;
+
+        return {
+            puerta1: porteria1Abierta ? 'Ingreso Por la Porteria 1' : 'Porteria 1 Cerrada',
+            puerta2: porteria2Abierta ? 'Ingreso Por la Porteria 2' : 'Porteria 2 Cerrada'
+        };
+    }
+
+    let puertaDeIngreso = ingreso();
+    $('#puerta_ingreso').html(`
+        <h5 class="center">${puertaDeIngreso.puerta1}</h5>
+        <h5 class="center">${puertaDeIngreso.puerta2}</h5>
+    `);
    
   
 
