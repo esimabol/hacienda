6c2564a1c97550699eb36040640a892b44633add
Eddy Simancas <edylber.simancas@outlook.com>
2025-06-03 22:20:59 -0500

    Configurando la seccion de documentos

--- assets/js/init.js
+++ assets/js/init.js
@@ -11,25 +11,28 @@
      $('.tooltipped').tooltip();
      $('.modal').modal();
     
-     function ingreso() {
-        let fecha = new Date();
-        let mes = fecha.getMonth() + 1;
-        let hora = fecha.getHours();
+    function ingreso() {
+      let fecha = new Date(); 
+      console.log(fecha);
+      let mes = fecha.getMonth() + 1;
+      let hora = fecha.getHours();
+      console.log(mes, hora);
 
-        let porteria1Abierta = mes % 2 === 0 || (hora >= 6 && hora < 22);
-        let porteria2Abierta = mes % 2 !== 0;
+      // Determinar cuál puerta está abierta las 24 horas y cuál tiene restricción nocturna
+      let puerta1Abierta = mes % 2 === 0; // Mes par -> puerta 1 abierta siempre
+      let puerta2Abierta = !puerta1Abierta; // Mes impar -> puerta 2 abierta siempre
 
-        return {
-            puerta1: porteria1Abierta ? 'Ingreso Por la Porteria 1' : 'Porteria 1 Cerrada',
-            puerta2: porteria2Abierta ? 'Ingreso Por la Porteria 2' : 'Porteria 2 Cerrada'
-        };
+      return {
+        puerta1: puerta1Abierta || (hora >= 6 && hora < 22) ? 'Ingreso Por la Porteria 1' : 'Porteria 1 Cerrada',
+        puerta2: puerta2Abierta || (hora >= 6 && hora < 22) ? 'Ingreso Por la Porteria 2' : 'Porteria 2 Cerrada'
+      };
     }
 
-    let puertaDeIngreso = ingreso();
-    $('#puerta_ingreso').html(`
-        <h5 class="center">${puertaDeIngreso.puerta1}</h5>
-        <h5 class="center">${puertaDeIngreso.puerta2}</h5>
-    `);
+  let puertaDeIngreso = ingreso();
+  $('#puerta_ingreso').html(`
+      <h5 class="center">${puertaDeIngreso.puerta1}</h5>
+      <h5 class="center">${puertaDeIngreso.puerta2}</h5>
+  `);
    
   
 
